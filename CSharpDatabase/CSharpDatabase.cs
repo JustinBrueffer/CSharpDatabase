7c418023c00dbc31c90f6c67b687ca8d81f9b035
JustinBrueffer <45484035+JustinBrueffer@users.noreply.github.com>
2018-12-10 22:23:35 +0100

    Anfang

---
+++ CSharpDatabase/CSharpDatabase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace CSharpDatabase
+{
+    [Serializable]
+    public class CSharpDatabase
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+        private string Name;
+
+        public CSharpDatabase()
+        {
+
+        }
+
+        public void Connect()
+        {
+
+        }
+
+        public void Create(string path)
+        {
+            bool overwrite = true;
+            //Check if file already exists
+            if (File.Exists(path) is true)
+            {
+                //Adapt to Console or GUI
+                if (GetConsoleWindow() != IntPtr.Zero)
+                {
+                    //Ask for Overwriting
+                    Console.WriteLine("Database is already existing.\nOverwrite it? [yes/no]");
+                    while (true)
+                    {
+                        Console.Clear();
+                        var choice = Console.ReadLine();
+                        if (choice.ToLower() == "yes")
+                        {
+                            overwrite = true;
+                            break;
+                        }
+                        else if (choice.ToLower() == "no")
+                        {
+                            overwrite = false;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("No valid option.");
+                        }
+                    }
+                }
+            }
+            if (overwrite is true)
+            {
+                //Create the Database
+                using (FileStream stream = File.Create(path))
+                {
+
+                }
+            }
+        }
+    }
+}
